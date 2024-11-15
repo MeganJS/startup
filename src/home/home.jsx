5d859dbe6ba67bcd63f4119afc09fdd34e9c430b
MeganJS <247megamania@gmail.com>
2024-11-14 18:59:16 -0700

    friend list effects put in place

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -6,11 +6,16 @@ import { useState, useEffect } from 'react';
 //components for projects, friendlist
 export function Home() {
     const [projects, setProjectList] = React.useState([]);
+    const [friends, setFriendList] = React.useState([]);
 
   // Demonstrates calling a service asynchronously so that
   // React can properly update state objects with the results.
   useEffect(() => {
     const pList = getProjectList();
+    const frList = getFriendList(); 
+    if (frList) {
+        setFriendList(frList);
+      }
     if (pList) {
       setProjectList(pList);
     }
@@ -32,7 +37,26 @@ export function Home() {
         <NavLink className='nav-link' to='/Project'>Create New Project</NavLink>
     </div>
   );
-  
+
+  const friendComps = [];
+  if (friends.length) {
+    for (const friend of friends) {
+      friendComps.push(
+        <li className="list-group-item">
+            <div>{friend}</div>
+            <button type="submit"><a href="home.html">disconnect</a></button>
+            <button type="submit"><a href="home.html">see shared projects</a></button>
+        </li>
+      );
+    }
+  }
+  friendComps.push(
+    <li className="list-group-item">
+        <label for="find-connection">Request Connection</label>
+        <input type="text" id="find-connection" pattern="\w{1,24}" required/>
+        <button type="submit"><a href="home.html">find</a></button>
+    </li>
+  );
 
   return (
     <main>
@@ -43,62 +67,18 @@ export function Home() {
             </section>
         </section>
         <section id="connections">
-            <h2>Connections</h2>
-            <div className="accordion" id="accordionExample">
-                <div className="accordion-item">
-                <h2 className="accordion-header">
-                    <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                        A. P. Eerson
-                    </button>
-                </h2>
-                <div id="collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
-                    <div className="accordion-body">
-                        <button type="submit"><a href="home.html">disconnect</a></button>
-                        <button type="submit"><a href="home.html">see shared projects</a></button>
-                    </div>
-                </div>
-                </div>
-                <div className="accordion-item">
-                <h2 className="accordion-header">
-                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                        Mr. Frogdatterson
-                    </button>
-                </h2>
-                <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
-                    <div className="accordion-body">
-                        <button type="submit"><a href="home.html">disconnect</a></button>
-                        <button type="submit"><a href="home.html">see shared projects</a></button>
-                    </div>
-                </div>
-                </div>
-                <div className="accordion-item">
-                <h2 className="accordion-header">
-                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                        Donna Noble
-                    </button>
-                </h2>
-                <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
-                    <div className="accordion-body">
-                        <button type="submit"><a href="home.html">disconnect</a></button>
-                        <button type="submit"><a href="home.html">see shared projects</a></button>
-                    </div>
-                </div>
-                
-                
-                </div>
-            </div>
             <ul className="list-group">
-                <li className="list-group-item">
-                    <label for="find-connection">Request Connection</label>
-                    <input type="text" id="find-connection" pattern="\w{1,24}" required/>
-                    <button type="submit"><a href="home.html">find</a></button>
-                </li>
+                {friendComps}
             </ul>
         </section>
 
     </main>
-
   );
+}
+
+function getFriendList() {
+    const frList = ["A. P. Eerson","Mr. Frogdatterson","Donna Noble"];
+    return frList;
 }
 
 function getProjectList() {
