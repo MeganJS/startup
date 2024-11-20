f17d5cf73975605a6edc61111704f69b10378db2
MeganJS <247megamania@gmail.com>
2024-11-19 22:41:15 -0700

    setup node.js and nodemon

--- service/index.js
+++ service/index.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const app = express();
+const uuid = require('uuid');
+
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('*', (_req, res) => {
+  res.send({ msg: 'idea-thing service' });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
