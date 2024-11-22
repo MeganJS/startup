35bdba289d534d0b79e80994702f557678497c00
MeganJS <247megamania@gmail.com>
2024-11-21 22:47:17 -0700

    added endpoints for logging, creating user

--- service/index.js
+++ service/index.js
@@ -17,15 +17,37 @@ app.use(`/api`, apiRouter);
 
 // CreateAuth a new user
 apiRouter.post('/auth/create', async (req, res) => {
-  const user = users[req.body.email];
+  const user = users[req.body.username];
   if (user) {
     res.status(409).send({ msg: 'Existing user' });
   } else {
-    const user = { email: req.body.email, password: req.body.password, projects: req.body.projects, friends: req.body.friends, token: uuid.v4() };
-    users[user.email] = user;
+    const user = { username: req.body.username, password: req.body.password, projects: req.body.projects, friends: req.body.friends, token: uuid.v4() };
+    users[user.username] = user;
 
     res.send({ token: user.token });
   }
+});
+
+// GetAuth login an existing user
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = users[req.body.username];
+  if (user) {
+    if (req.body.password === user.password) {
+      user.token = uuid.v4();
+      res.send({ token: user.token });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// DeleteAuth logout a user
+apiRouter.delete('/auth/logout', (req, res) => {
+  const user = Object.values(users).find((u) => u.token === req.body.token);
+  if (user) {
+    delete user.token;
+  }
+  res.status(204).end();
 });
 
 app.get('*', (_req, res) => {
