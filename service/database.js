ab1ad1ee8e19d676fb745badcd020e4ce05c9eec
MeganJS <247megamania@gmail.com>
2024-11-27 21:26:36 -0700

    added endpoints to database and index for login

--- service/database.js
+++ service/database.js
@@ -1,10 +1,12 @@
 const { MongoClient } = require('mongodb');
 const config = require('./dbConfig.json');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
-
 const client = new MongoClient(url);
-
-const db = client.db('rental');
+const db = client.db('idea-thing');
+const userColl = db.collection('user');
 
 (async function testConn() {
     await client.connect();
@@ -13,3 +15,31 @@ const db = client.db('rental');
     console.log(`Unable to connect to database with ${url} because ${ex.message}`);
     process.exit(1);
 });
+
+function getUser(username) {
+    return userColl.findOne({ username: username });
+}
+
+function getUserByToken(token) {
+    return userColl.findOne({token: token});
+}
+
+async function createUser(username, password) {
+    const passHash = await bcrypt.hash(password, 10);
+    const user = {
+        username: username,
+        password: passHash,
+        token: uuid.v4(),
+        projects: [],
+        friends: []
+    };
+    await userColl.insertOne(user);
+
+    return user;
+}
+
+module.exports = {
+    getUser,
+    getUserByToken,
+    createUser
+};
--- service/index.js
+++ service/index.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const app = express();
 const uuid = require('uuid');
-
+const bcrypt = require('bcrypt');
+const DB = require('./database.js');
+const cookieParser = require('cookie-parser');
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
+const auth = "token";
 
 let users = {};
 let curUser = {};
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.static('public'));
+
 
 // Router for service endpoints
 var apiRouter = express.Router();
@@ -17,26 +22,29 @@ app.use(`/api`, apiRouter);
 //todo: look into cookie
 // CreateAuth a new user
 apiRouter.post('/auth/create', async (req, res) => {
-  const user = users[req.body.username];
+  let user = await DB.getUser(req.body.username);
   if (user) {
     res.status(409).send({ msg: 'Existing user' });
   } else {
-    const user = { username: req.body.username, password: req.body.password, projects: req.body.projects, friends: req.body.friends, token: uuid.v4() };
-    users[user.username] = user;
-    curUser=user;
-
-    res.send({ token: user.token });
+    const user = await DB.createUser(req.body.username, req.body.password);
+    res.cookie(auth, user.token, {secure:true, httpOnly: true, sameSite: 'strict'});
+    //res.send({ token: user.token });
+    //res.send({id: user._id}); ???
   }
 });
 
 // GetAuth login an existing user
 apiRouter.post('/auth/login', async (req, res) => {
-  const user = users[req.body.username];
+  let user = await DB.getUser(req.body.username);
   if (user) {
-    if (req.body.password === user.password) {
-      user.token = uuid.v4();
-      curUser=user;
-      res.send({ token: user.token });
+    let hashPass = await bcrypt.compare(req.body.password, user.password);
+    if (hashPass) {
+      res.cookie(auth, user.token, {secure:true, httpOnly: true, sameSite: 'strict'});
+      //res.send({id: user._id});
+
+      //user.token = uuid.v4();
+      //curUser=user;
+      //res.send({ token: user.token });
       return;
     }
   }
@@ -44,37 +52,53 @@ apiRouter.post('/auth/login', async (req, res) => {
 });
 
 // DeleteAuth logout a user
-apiRouter.delete('/auth/logout', (req, res) => {
+apiRouter.delete('/auth/logout', (_req, res) => {
+  res.clearCookie(auth);
+  /*
   const user = Object.values(users).find((u) => u.token === req.body.token);
   if (user) {
     delete user.token;
     curUser = {};
   }
+  */
   res.status(204).end();
+});
+
+const secureRouter = express.Router();
+apiRouter.use(secureRouter);
+
+secureRouter.use(async (req, res, next) => {
+  const authToken = req.cookies[auth];
+  const user = await DB.getUserByToken(authToken);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({msg: 'Unauthorized' });
+  }
 });
 
 
 // Getprojects note: req may need to be _req???
 // how to check if user is logged in?
-apiRouter.get('/projects', (_req, res) => {
-  const user = users[curUser.username];
-  //const user = Object.values(users).find((u) => u.token === req.body.token);
-  if (user){
+secureRouter.get('/projects', async (req, res) => {
+  const authToken = req.cookies[auth];
+  const user = await DB.getUserByToken(authToken);
+  if (user) {
     res.send(user.projects);
-    return;
+  } else {
+    res.status(401).send({msg: 'Unauthorized' });
   }
-  res.status(401).send({ msg: 'Unauthorized' });
 });
 
 // Getfriends note: req may need to be _req???
-apiRouter.get('/friends', (req, res) => {
-  const user = users[curUser.username];
-  //const user = Object.values(users).find((u) => u.token === req.body.token);
-  if (user){
+apiRouter.get('/friends', async (req, res) => {
+  const authToken = req.cookies[auth];
+  const user = await DB.getUserByToken(authToken);
+  if (user) {
     res.send(user.friends);
-    return;
+  } else {
+    res.status(401).send({msg: 'Unauthorized' });
   }
-  res.status(401).send({ msg: 'Unauthorized' });
 });
 
 
